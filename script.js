1a7d3a979682308e609eb9acf8aa4e34882403f4
ShreyaKurry13 <shreyakurry@gmail.com>
2022-09-27 01:20:14 +0530

    bfit website

---
+++ script.js
@@ -0,0 +1,18 @@
+    var btn = document.getElementById ( ' btn ' ) ;
+    function leftClick()
+    {
+         btn.style.left = '0';
+   }
+   function rightClick()
+   {
+        btn.style.left='110px';
+   }
+
+//    var modal = document.getElementById('login-form') ;
+//    window.onclick = function ( event )
+//    {
+//        if ( event.target == modal )
+//        {
+//            modal.style.display = " none " ;
+//        }
+//    }

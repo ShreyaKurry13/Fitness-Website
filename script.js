fa23096bfbb90fb083268e62a2dbe569b900ca9a
ShreyaKurry13 <shreyakurry@gmail.com>
2022-10-02 22:05:17 +0530

    [code changes]: 1. created contact page 2.created home page 3. made changes in stylesheet 4. added a new image in images folder 5. modifies the JS file 6. modifies the stylesheet of lin page 7. deleted the extra index.html page 8. deleted the unused images from the images folder

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-    var btn = document.getElementById ( ' btn ' ) ;
-    function leftClick()
-    {
-         btn.style.left = '0';
-   }
-   function rightClick()
-   {
-        btn.style.left='110px';
-   }
+const login = document.querySelector(".login");
+const register = document.querySelector(".register");
+const form = document.querySelector("#form");
+// const form = document.getElementById("#form");
+const switchs = document.querySelector(".switch");
 
-//    var modal = document.getElementById('login-form') ;
-//    window.onclick = function ( event )
-//    {
-//        if ( event.target == modal )
-//        {
-//            modal.style.display = " none " ;
-//        }
-//    }
+// var button = document.getElementById("#btnMain")
+
+function rightClick(){
+  document.getElementById('form').style.marginLeft='-100%';
+  // form.style.marginLeft = "-100%";
+  // login.style.background = "none";
+  // register.style.background = "orangered"; 
+  document.getElementById("btnMain").style.left = "110px";
+}
+function leftClick(){
+  document.getElementById('form').style.marginLeft='0';
+  // register.style.background = "none";
+  // login.style.background = "orangered"; 
+  document.getElementById("btnMain").style.left = "0";
+}
+
+
+
+// // var btn = document.getElementById("btn");
+
+// // function leftClick() {
+// //   btn.style.left = "1";
+// // }
+// // function rightClick() {
+// //   btn.style.left = "110px";
+// // }
+
+
+// //    var modal = document.getElementById('login-form') ;
+// //    window.onclick = function ( event )
+// //    {
+// //        if ( event.target == modal )
+// //        {
+// //            modal.style.display = " none " ;
+// //        }
+// //    }
